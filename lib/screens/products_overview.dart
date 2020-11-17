89655b9db091ac9b3bb33cdef01f1e79093373bb
Tung Huynh <htqtung@gmail.com>
2020-11-17 13:34:25 +0200

    Working favorite functionality

--- lib/screens/products_overview.dart
+++ lib/screens/products_overview.dart
@@ -2,15 +2,53 @@ import 'package:flutter/material.dart';
 
 import '../widgets/products_grid.dart';
 
-class ProductsOverviewScreen extends StatelessWidget {
+enum FilterOptions {
+  Favorites,
+  All,
+}
+
+class ProductsOverviewScreen extends StatefulWidget {
+  @override
+  _ProductsOverviewScreenState createState() => _ProductsOverviewScreenState();
+}
+
+class _ProductsOverviewScreenState extends State<ProductsOverviewScreen> {
+  var _showOnlyFavorites = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Products'),
         centerTitle: true,
+        actions: <Widget>[
+          PopupMenuButton(
+            itemBuilder: (_) => [
+              PopupMenuItem(
+                child: Text('Favorites'),
+                value: FilterOptions.Favorites,
+              ),
+              PopupMenuItem(
+                child: Text('All products'),
+                value: FilterOptions.All,
+              ),
+            ],
+            icon: Icon(
+              Icons.more_vert,
+            ),
+            onSelected: (FilterOptions selectedValue) {
+              setState(() {
+                if (selectedValue == FilterOptions.Favorites) {
+                  _showOnlyFavorites = true;
+                } else {
+                  _showOnlyFavorites = false;
+                }
+              });
+            },
+          )
+        ],
       ),
-      body: ProductsGrid(),
+      body: ProductsGrid(_showOnlyFavorites),
     );
   }
 }
--- lib/widgets/products_grid.dart
+++ lib/widgets/products_grid.dart
@@ -3,13 +3,17 @@ import 'package:provider/provider.dart';
 
 import '../widgets/product_item.dart';
 import '../providers/products.dart';
-import '../providers/product.dart';
 
 class ProductsGrid extends StatelessWidget {
+  final bool showFavorites;
+
+  ProductsGrid(this.showFavorites);
+
   @override
   Widget build(BuildContext context) {
     final productsData = Provider.of<Products>(context);
-    final products = productsData.items;
+    final products =
+        showFavorites ? productsData.favoriteItems : productsData.items;
     return GridView.builder(
       padding: const EdgeInsets.all(12.0),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
