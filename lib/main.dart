662caecf8dfa25f11b4029cffc8c6b03e1edf4a4
Tung Huynh <htqtung@gmail.com>
2021-01-20 08:59:52 +0200

    add login/signup screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_online_shop/screens/orders_screen.dart';
 import 'package:provider/provider.dart';
 
 import './screens/product_detail.dart';
 import './screens/products_overview_screen.dart';
+import './providers/auth.dart';
 import './providers/products.dart';
 import './providers/cart.dart';
 import './providers/orders.dart';
 import './screens/cart_screen.dart';
 import './screens/user_products_screen.dart';
 import './screens/edit_product_screen.dart';
+import './screens/auth_screen.dart';
+import './screens/orders_screen.dart';
 
 void main() => runApp(MyApp());
 
@@ -17,7 +19,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MultiProvider(
+      // recommended way
+      // use create instead of .value to instantiate a new object
+      // for efficiency and avoiding bugs
+      // this is different from List/Grid items where we reusing widgets
+      // with existing data
       providers: [
+        ChangeNotifierProvider(
+          create: (ctx) => Auth(),
+        ),
         ChangeNotifierProvider(
           create: (ctx) => Products(),
         ),
@@ -26,28 +36,27 @@ class MyApp extends StatelessWidget {
         ),
         ChangeNotifierProvider(
           create: (ctx) => Orders(),
-        )
+        ),
       ],
-      // recommended way
-      // use create instead of .value to instantiate a new object
-      // for efficiency and avoiding bugs
-      // this is different from List/Grid items where we reusing widgets
-      // with existing data
-      child: MaterialApp(
-          title: 'iShop',
-          theme: ThemeData(
-            primarySwatch: Colors.deepOrange,
-            accentColor: Colors.amber,
-            fontFamily: 'Lato',
-          ),
-          home: ProductsOverviewScreen(),
-          routes: {
-            ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
-            CartScreen.routeName: (ctx) => CartScreen(),
-            OrdersScreen.routeName: (ctx) => OrdersScreen(),
-            UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
-            EditProductScreen.routeName: (ctx) => EditProductScreen(),
-          }),
+      // MaterialApp will be rebuilt whenever auth changes
+      child: Consumer<Auth>(
+        builder: (ctx, auth, _) => MaterialApp(
+            title: 'iShop',
+            theme: ThemeData(
+              primarySwatch: Colors.deepOrange,
+              accentColor: Colors.amber,
+              fontFamily: 'Lato',
+            ),
+            home:
+                auth.isAuthenticated ? ProductsOverviewScreen() : AuthScreen(),
+            routes: {
+              ProductDetailScreen.routeName: (ctx) => ProductDetailScreen(),
+              CartScreen.routeName: (ctx) => CartScreen(),
+              OrdersScreen.routeName: (ctx) => OrdersScreen(),
+              UserProductsScreen.routeName: (ctx) => UserProductsScreen(),
+              EditProductScreen.routeName: (ctx) => EditProductScreen(),
+            }),
+      ),
     );
   }
 }
---
+++ lib/providers/auth.dart
@@ -0,0 +1,64 @@
+import 'dart:convert';
+
+import 'package:flutter/widgets.dart';
+import 'package:http/http.dart' as http;
+import '../models/http_exception.dart';
+
+class Auth with ChangeNotifier {
+  String _token;
+  DateTime _tokenExpDate;
+  String _userId;
+
+  bool get isAuthenticated {
+    return token != null;
+  }
+
+  String get token {
+    if (_tokenExpDate != null &&
+        _tokenExpDate.isAfter(DateTime.now()) &&
+        _token != null) {
+      return _token;
+    }
+    return null;
+  }
+
+  Future<void> _authenticate(
+      String email, String password, String urlSegment) async {
+    final url =
+        'https://identitytoolkit.googleapis.com/v1/accounts:$urlSegment?key=[API]';
+    try {
+      final response = await http.post(
+        url,
+        body: json.encode({
+          'email': email,
+          'password': password,
+          'returnSecureToken': true,
+        }),
+      );
+      final responseData = json.decode(response.body);
+      if (responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+      _token = responseData['idToken'];
+      _userId = responseData['localId'];
+      _tokenExpDate = DateTime.now().add(Duration(
+        seconds: int.parse(
+          responseData['expiresIn'],
+        ),
+      ));
+      notifyListeners();
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  Future<void> signUp(String email, String password) async {
+    // The future that is returned by _authenticate is the future that waits
+    // for the http request, so we need to return that one, not the future of signUp()
+    return _authenticate(email, password, 'signUp');
+  }
+
+  Future<void> signIn(String email, String password) async {
+    return _authenticate(email, password, 'signInWithPassword');
+  }
+}
